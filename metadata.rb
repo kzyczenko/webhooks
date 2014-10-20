e356d8807ba5984e71438838ee345e43e8a119c8
stajkowski <stajkowski100@gmail.com>
2014-10-20 08:52:51 -0700

    Developing Resources

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'webhooks'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Brian Stajkowski'
+maintainer_email 'stajkowski'
 license          'All rights reserved'
-description      'Installs/Configures webhooks'
+description      'LWRP Providers for API Calls or Hooks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
---
+++ resources/http.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+
+actions :get, :post, :put
+default_action :get if defined?(default_action)
+
+#Default name for operation.  Not used for other than resource name.
+attribute :operation_name, :name_attribute => true, :kind_of => String, :required => true
+
+#Base Required Options
+attribute :uri, :kind_of => String, :required => true, :default => nil
+attribute :uri_port, :kind_of => Integer, :required => false, :default => 80
+attribute :expected_response_code, :kind_of => Integer, :required => false, :default => 200
+attribute :follow_redirect, :kind_of => [ TrueClass, FalseClass ], :default => false
+attribute :read_timeout, :kind_of => Integer, :required => false, :default => 60
+attribute :use_ssl, :kind_of => [ TrueClass, FalseClass ], :default => false
+attribute :post_data, :kind_of => Hash, :required => false, :default => nil
+attribute :save_response, :kind_of => [ TrueClass, FalseClass ], :default => true
+
+#Basic Authentication
+attribute :use_basic_auth, :kind_of => [ TrueClass, FalseClass ], :default => false
+attribute :basic_auth_username, :kind_of => String, :required => false, :default => nil
+attribute :basic_auth_password, :kind_of => String, :required => false, :default => nil
+
+#Proxy Options
+attribute :use_proxy, :kind_of => [ TrueClass, FalseClass ], :default => false
+attribute :proxy_address, :kind_of => String, :required => false, :default => nil
+attribute :proxy_port, :kind_of => Integer, :required => false, :default => nil
+attribute :proxy_username, :kind_of => String, :required => false, :default => nil
+attribute :proxy_password, :kind_of => String, :required => false, :default => nil
+
+attr_accessor :exists
